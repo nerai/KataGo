fd96b574f6a46b1736ba17a7ba67c162ba2c0b6c
Sebastian Heuchler <git@mafutrct.de>
2023-05-24 21:45:19 +0200

    .

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -169,13 +169,20 @@ static int handleSubcommand(const string& subcommand, const vector<string>& args
 }
 
 
+#include <filesystem>
 int main(int argc, const char* const* argv) {
   vector<string> args = MainArgs::getCommandLineArgsUTF8(argc,argv);
   MainArgs::makeCoutAndCerrAcceptUTF8();
 
   if(args.size() < 2) {
-    printHelp(args);
-    return 0;
+    //printHelp(args);
+    //return 0;
+      std::cout << "CWD: " << std::filesystem::current_path() << "\n";
+      args.push_back("benchmark");
+      args.push_back("-model");
+      args.push_back("../../../KataGo2/default_model.bin.gz");
+      args.push_back("-config");
+      args.push_back("../configs/gtp_example.cfg");
   }
   string cmdArg = string(args[1]);
   if(cmdArg == "-h" || cmdArg == "--h" || cmdArg == "-help" || cmdArg == "--help" || cmdArg == "help") {
